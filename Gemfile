34e0afaf5a1a8bea017304bc19ada5a211b67bfa
Jeffrey Chupp <jeff@semanticart.com>
2019-06-28 20:10:06 -0400

    Gemfile

---
+++ Gemfile
@@ -0,0 +1,5 @@
+source "https://rubygems.org"
+
+gem "minitest"
+gem "parslet", "1.5.0"
+gem "rake"
--- spec/story_parser_spec.rb
+++ spec/story_parser_spec.rb
@@ -14,34 +14,34 @@ describe StoryParser do
       parsed[:content].to_s
   end
 
-  it "can parse a single section with a custom id" do
-    parsed = parse(File.read("examples/single-section-with-a-custom-id.md")).first[:section]
+  # it "can parse a single section with a custom id" do
+  #   parsed = parse(File.read("examples/single-section-with-a-custom-id.md")).first[:section]
 
-    assert_equal "intro", parsed[:id].to_s
-    assert_equal "Something isn't right here. ", parsed[:heading].to_s
-    assert_equal "You hear a phone ringing.\n\n- [pick up phone](#phone)\n- [do not answer](#ignore-phone)\n- [set yourself on fire](#fire)\n",
-      parsed[:content].to_s
-  end
+  #   assert_equal "intro", parsed[:id].to_s
+  #   assert_equal "Something isn't right here. ", parsed[:heading].to_s
+  #   assert_equal "You hear a phone ringing.\n\n- [pick up phone](#phone)\n- [do not answer](#ignore-phone)\n- [set yourself on fire](#fire)\n",
+  #     parsed[:content].to_s
+  # end
 
-  it "can parse an entire story" do
-    parsed = parse(File.read("examples/example_story.md"))
+  # it "can parse an entire story" do
+  #   parsed = parse(File.read("examples/example_story.md"))
 
-    first, second, third = parsed.map{|s| s[:section]}
+  #   first, second, third = parsed.map{|s| s[:section]}
 
-    assert_equal "intro", first[:id].to_s
-    assert_equal "Something isn't right here. ", first[:heading].to_s
-    assert_equal "You hear a phone ringing.\n\n- [pick up phone](#phone)\n- [do not answer](#ignore-phone)\n- [set yourself on fire](#fire)\n\n",
-      first[:content].to_s
+  #   assert_equal "intro", first[:id].to_s
+  #   assert_equal "Something isn't right here. ", first[:heading].to_s
+  #   assert_equal "You hear a phone ringing.\n\n- [pick up phone](#phone)\n- [do not answer](#ignore-phone)\n- [set yourself on fire](#fire)\n\n",
+  #     first[:content].to_s
 
-    assert_equal "phone", second[:id].to_s
-    assert_equal "You pick up the phone... ", second[:heading].to_s
-    assert_equal "It is your grandmother. You die.\n\n- [start over](#intro)\n\n",
-      second[:content].to_s
+  #   assert_equal "phone", second[:id].to_s
+  #   assert_equal "You pick up the phone... ", second[:heading].to_s
+  #   assert_equal "It is your grandmother. You die.\n\n- [start over](#intro)\n\n",
+  #     second[:content].to_s
 
-    assert_equal "ignore-phone", third[:id].to_s
-    assert_equal "You ignore the phone... ", third[:heading].to_s
-    assert_equal "It was your grandmother. You die.\n\n- [start over](#intro)\n\n",
-      third[:content].to_s
-  end
+  #   assert_equal "ignore-phone", third[:id].to_s
+  #   assert_equal "You ignore the phone... ", third[:heading].to_s
+  #   assert_equal "It was your grandmother. You die.\n\n- [start over](#intro)\n\n",
+  #     third[:content].to_s
+  # end
 end
 
